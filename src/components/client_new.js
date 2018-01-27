6bf681dd58c176570bf5d4f97e3c1576643e5814
hnefatafl01 <kevin.erick01@gmail.com>
2018-01-26 17:50:49 -0700

    center form box

--- src/components/client_new.js
+++ src/components/client_new.js
@@ -5,9 +5,12 @@ import { connect } from 'react-redux';
 import { createClient } from '../actions/index';
 
 class ClientNew extends Component {
+
     renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className = `form-group ${touched && error ? 'input-error' : '' }`;
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                     placeholder="add client name"
@@ -15,6 +18,7 @@ class ClientNew extends Component {
                     type="text"
                     { ...field.input }
                 />
+                <div className="form-text text-danger">{ touched ? error : '' }</div>
             </div>
         );
     }
@@ -26,19 +30,22 @@ class ClientNew extends Component {
     }
 
     render() {
-        const { handleSubmit } = this.props;
+        const { handleSubmit, invalid } = this.props;
+
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-md-3">
-                        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-                            <Field
-                                label="Client Name"
-                                name="name"
-                                component={this.renderField} />
-                            <button className="btn btn-success">Save</button>
-                            <Link to="/clients" className="btn btn-danger">Cancel</Link>
-                        </form>
+                    <div className="col-12 col-md-4 offset-md-4">
+                        <div className="form-box">
+                            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                                <Field
+                                    label="Client Name"
+                                    name="name"
+                                    component={this.renderField} />
+                                <button className="btn btn-success" disabled={ invalid }>Save</button>
+                                <Link to="/clients" className="btn btn-danger">Cancel</Link>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -46,7 +53,18 @@ class ClientNew extends Component {
     }
 }
 
+function validate(values) {
+    const errors = {}; //if no properties form is valid
+
+    if (!values.name) {
+        errors.name = "Add a name!";
+    }  
+    
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'ClientsNewForm'
 })(
     connect(null, { createClient })(ClientNew)
