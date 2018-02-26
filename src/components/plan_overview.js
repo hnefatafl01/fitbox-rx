1b4025c17b6de2e83908cbc3b4cab0f0c20cc849
hnefatafl01 <kevin.erick01@gmail.com>
2018-02-25 20:08:34 -0700

    toggle add form

--- src/components/plan_overview.js
+++ src/components/plan_overview.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+// import { connect } from 'react-redux';
 import { Sparklines, SparklinesLine } from 'react-sparklines';
 import { Plus } from 'react-feather';
+import SessionNew from './session_new';
 
 export default class PlanOverview extends Component {
   constructor(props) {
@@ -20,16 +21,24 @@ export default class PlanOverview extends Component {
   }
   
   render() {
+    var addNewSession;
+    if(this.state.showAdd === true) {
+      addNewSession = <SessionNew />;
+    } else {
+      addNewSession = <br/>;
+    }
+
     return (
       <div className="container">
         <Sparklines data={[5, 10, 5, 20]}>
           <SparklinesLine color="blue" />
         </Sparklines>
         <div className="d-flex justify-content-between">
-          <button className="btn btn-outline-info" onClick={this.handleToggleAdd}>
+          <button className="btn btn-outline-info" style={{'marginTop':'20px'}} onClick={this.handleToggleAdd}>
             <Plus style={{'verticalAlign': 'middle'}}/>
           </button>
         </div>
+        {addNewSession}
       </div>
     )
   }
